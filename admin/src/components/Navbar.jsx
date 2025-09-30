dee722f88e3d79bbb0591421740d58c69be1a080
mdsufiyanansari <mdsufiyanansari812002@gmail.com>
2025-09-30 21:50:40 +0530

    refactor: Comment out Logout button in Navbar component

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -12,11 +12,11 @@ const Navbar = ({setToken}) => {
           </div>
 
           {/* Navigation / Buttons */}
-          <div className="flex items-center space-x-4">
+          {/* <div className="flex items-center space-x-4">
             <button onClick={()=>setToken("")} className="bg-red-500 hover:bg-red-600 text-white font-semibold px-4 py-2 rounded-lg shadow-md transition-all duration-200">
               Logout
             </button>
-          </div>
+          </div> */}
         </div>
       </div>
     </nav>
