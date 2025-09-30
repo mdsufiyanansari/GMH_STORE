f0794b9db7c57a791e225281c6803cc19eda458f
mdsufiyanansari <mdsufiyanansari812002@gmail.com>
2025-09-30 21:33:59 +0530

    Update assets and middleware configuration
    - Removed obsolete CSS file: index-Zyel5aZT.css
    - Updated HTML to reference new JavaScript and CSS files: index-CaUEqwoC.js and index-BabmcMpa.css
    - Added new allowed origin to CORS middleware for enhanced security

--- backend/middleware/corsMiddleware.js
+++ backend/middleware/corsMiddleware.js
@@ -4,6 +4,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "https://gmh-store.vercel.app",
+   "https://gmh-store-admin-f4txah0xh-mdsufiyanansaris-projects.vercel.app"
 ];
 
 const corsOptions = {
